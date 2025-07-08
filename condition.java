ef0550b38e911900d295f9b16c599d8daab3928c
Rohit Kumar Yadav <your_email@example.com>
2025-07-08 06:19:28 +0530

    Dsa placement

--- condition.java
+++ condition.java
@@ -1,26 +1,65 @@
-import java.util.*;
 public class condition {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the number : ");
-        int a=sc.nextInt();
-        /// condition check//
-        if(a>=0){
-            System.out.println("Positive");
+    //     Scanner sc=new Scanner(System.in);
+    //     System.out.print("Enter the Day number : ");
+    //     double dayofweek=sc.nextInt();
+    //    // short  circuit &&
+    //     // if(dayofweek < 1 || dayofweek >7){
+    //     //     System.out.print("Invalid number:");
+    //     // }
+    //     if(dayofweek==1){
+    //         System.out.println("Monday");
+    //     }
+    //     else if(dayofweek==2){
+    //         System.out.println("Tuesday");
+    //     }
+    //      else if(dayofweek==3){
+    //         System.out.println("wednesday");
+    //      }
+    //      else if(dayofweek==4){
+    //         System.out.println("thursday");
+    //      }
+    //      else if(dayofweek==5){
+    //         System.out.println("friday");
+    //      }
+    //      else if (dayofweek==6){
+    //         System.out.println("saturday");
+    //      }
+    //        else if(dayofweek==7){
+    //         System.out.println("sunday");
+    //        }
+    //        else{
+    //         System.out.println("Invalid number");
+    //        }
+          
 
-        }   
-        else{
-            System.out.println("Negative");
-        }
+    //     System.out.println("Enter the number : ");
+    //     int a=sc.nextInt();
+    //     /// condition check//
+    //     if(a>=0){
+    //         System.out.println("Positive");
+
+    //     }   
+    //     else{
+    //         System.out.println("Negative");
+    //     }
     
    
-    double temp=103.5;  // reason error  By default 
-    if(temp>100){
-        System.out.println("have fever");
-    }
-    else{
-        System.out.println("no fever");
-    }
+    // double temp=103.5;  // reason error  By default 
+    // if(temp>100){
+    //     System.out.println("have fever");
+    // }
+    // else{
+    //     System.out.println("no fever");
+    // }
+    // terniary operator
+// int a=(5>9)?1:2;
+// System.out.println(a);
+int   a=63, b=36;
+boolean x = (a<b) ? true:false;
+int y=(a>b)?a:b;
+System.out.println(x);
+System.out.println(y);
 
 
     }
