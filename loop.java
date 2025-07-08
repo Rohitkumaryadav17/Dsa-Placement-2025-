0e1d46fead3e7e217a02e5c8db0e00cac58a7f3e
Rohit Kumar Yadav <your_email@example.com>
2025-07-08 19:26:49 +0530

    Dsa

--- loop.java
+++ loop.java
@@ -1,10 +1,38 @@
+import java.util.*;
 public class loop {
-    public static void main(String[] args) {
-        for(int i=0;i<5;i++){
-            System.out.println("hello");
-            i+=2;
-        }
-        
+public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+        // for(int i=0;i<5;i++){
+        //     System.out.println("hello");
+        //     i+=2;
+        // }
+        // for(int i=0;i<=200;i++){
+        //     System.out.print("hello");
+
+        // }
+        // while loop
+        // int a=0;
+        // while(a<10){
+        //     System.out.println("hello");
+        //     a++;
+        // }
+        //  print all even number bt 1 to 100
+    //  int i=0;
+    //  while(i<=100){
+    //     if(i%2==0){
+    //         System.out.println(i);
+    //         i++;
+    //   }  }
+// for(int i=5;i<=50;i=i+5){
+// System.out.println(i);
+   // }
+
+
     }
-    
-}
+
+     }
+        
+//loop concept
+//for loop 
+//whileloop   
+
